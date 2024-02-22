8aa96e132005181a254a051560064dffcf4af66b
Nguyen Thanh Phong <96514239+TrH-In-GsH@users.noreply.github.com>
2024-02-22 18:47:02 +0700

    Add files via upload

---
+++ es6.js
@@ -0,0 +1,6 @@
+// let age = 20;
+// age='bala'
+// console.log(age);
+let arr = [1,2,3,'bala'] ;
+arr[4]='phap';
+console.log(arr);
---
+++ es6.ts
@@ -0,0 +1,3 @@
+let ourTuple: [number, boolean, string];
+ ourTuple = [1,true,"hello"]
+console.log(ourTuple);
